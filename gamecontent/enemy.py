19206ac9adb9ba4cc20b230b8de4e4f0c28b8509
Kian1991 <kianluetke@gmail.com>
2017-03-25 11:32:06 +0000

    enemy disappear implented

--- gamecontent/enemy.py
+++ gamecontent/enemy.py
@@ -55,14 +55,17 @@ class spawner(threading.Thread):
                                         lc.cursor_pos = (x, i + 1)
                                         lc.write_string(" ")
                                 lc.cursor_pos = (x, i)
-                                lc.write_string(chr(1))
-                                time.sleep(level[gp.current])##Speed of enemys depends on current level
 				if i == 0:
-					if gp.positon == (x, i):##check collidsion
-						sys.exit()
-					else:##delete enemy
-                                                lc.cursor_pos = (x, i)
-						lc.write_string(' ')
+					#if gp.positon == (x, i):##check collidsion
+					#	sys.exit()
+					#else:##delete enemy
+					lc.write_string(chr(1))
+					time.sleep(0.2)
+					lc.cursor_pos = (x, i)
+					lc.write_string(' ')
+				else:
+					lc.write_string(chr(1))
+				time.sleep(level[gp.current])
                         else:
                                 self.stop()
                 
